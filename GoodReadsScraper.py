9a0624d7901a1b701a170f38d380a61ac9966630
James Cooper <jcooper333@hotmail.com>
2023-11-29 17:20:33 -0500

    Added selecting for each item

--- GoodReadsScraper.py
+++ GoodReadsScraper.py
@@ -6,5 +6,9 @@ url = 'https://www.goodreads.com/list/show/35857.The_Most_Popular_Fantasy_on_Goo
 
 r = s.get(url)
 
-print (r.text)
+Books = (r.html.find('#all_votes tr'))
 
+#Domain = "https://www.goodreads.com"
+# I will add to join this with the for loop's results
+for item in Books:
+    print (item.find('a', first=True).attrs['href'])
