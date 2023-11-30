39976dd23ddf7de320c086b31286f6c9e01f17dc
James Cooper <jcooper333@hotmail.com>
2023-11-29 21:33:43 -0500

    Can now collect info from each url in the page

--- GoodReadsScraper.py
+++ GoodReadsScraper.py
@@ -13,10 +13,23 @@ def get_book_links(page):
         links.append(Domain+(item.find('a', first=True).attrs['href']))
     return links 
 
-Test_Link = 'https://www.goodreads.com/book/show/186074.The_Name_of_the_Wind'
+def parse_book(url):
+        
+    r = s.get(url)
 
-r = s.get(Test_Link)
+    BookName = r.html.find('h1.Text.Text__title1', first=True).text
+    BookAuthor = r.html.find('span.ContributorLink__name', first=True).text
+    BookRating = r.html.find('div.RatingStatistics__rating', first=True).text
+    BookGenre = r.html.find('ul.CollapsableList', first=True).text
 
-BookName = r.html.find('h1.Text.Text__title1', first=True).text
-BookAuthor = r.html.find('span.ContributorLink__name', first=True).text
-BookRating = r.html.find('div.RatingStatistics__rating', first=True).text
+    book = {
+        'BookName': BookName,
+        'BookAuthor': BookAuthor,
+        'BookRating': BookRating,
+        'BookGenre': BookGenre
+        }   
+    return book
+
+urls = get_book_links(1)
+for url in urls:
+        print(parse_book(url))
