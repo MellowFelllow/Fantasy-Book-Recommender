749a9286efd669bac2103f8da290db0aad0bb408
James Cooper <jcooper333@hotmail.com>
2023-11-30 12:28:29 -0500

    Changed data type from list to tuples

--- GoodReadsScraper.py
+++ GoodReadsScraper.py
@@ -16,20 +16,21 @@ def get_book_links(page):
 def parse_book(url):
         
     r = s.get(url)
-
     BookName = r.html.find('h1.Text.Text__title1', first=True).text
     BookAuthor = r.html.find('span.ContributorLink__name', first=True).text
     BookRating = r.html.find('div.RatingStatistics__rating', first=True).text
     BookGenre = r.html.find('ul.CollapsableList', first=True).text.replace('\n','').replace('...more','').replace('Genres','').split()
 
-    book = {
+    book = (
         'BookName': BookName,
         'BookAuthor': BookAuthor,
         'BookRating': BookRating,
         'BookGenre': BookGenre
-        }   
+        )
+      
     return book
 
-urls = get_book_links(1)
-for url in urls:
-        print(parse_book(url))
+#Pagination - Saving for later
+#urls = get_book_links(1)
+#for url in urls:
+#        print(parse_book(url))
