a687c5eaf18a059e0d3f035b098e43466fe4b365
James Cooper <jcooper333@hotmail.com>
2023-11-29 20:09:07 -0500

    Built out section collecting urls

--- GoodReadsScraper.py
+++ GoodReadsScraper.py
@@ -2,13 +2,14 @@ from requests_html import HTMLSession
 
 s = HTMLSession()
 
-url = 'https://www.goodreads.com/list/show/35857.The_Most_Popular_Fantasy_on_Goodreads?page=1'
+def get_book_links(page):
+    url = f'https://www.goodreads.com/list/show/35857.The_Most_Popular_Fantasy_on_Goodreads?page={page}'
+    links = []
+    r = s.get(url)
+    Books = (r.html.find('#all_votes tr'))
+    Domain = "https://www.goodreads.com"
 
-r = s.get(url)
+    for item in Books:
+        links.append(Domain+(item.find('a', first=True).attrs['href']))
+    return links 
 
-Books = (r.html.find('#all_votes tr'))
-
-#Domain = "https://www.goodreads.com"
-# I will add to join this with the for loop's results
-for item in Books:
-    print (item.find('a', first=True).attrs['href'])
